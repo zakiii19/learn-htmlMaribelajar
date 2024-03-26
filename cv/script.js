b4dee6be3ca8e955b5395b1b63392b4a8c99e252
zakiii19 <qolbulmuzaki@gmail.com>
2024-03-27 00:30:56 +0700

    nw

---
+++ cv/script.js
@@ -0,0 +1,8 @@
+const nikElement = document.getElementById("nik");
+const namaElement = document.getElementById("nama");
+const ttlElement = document.getElementById("ttl");
+
+// Masukkan data diri Anda di sini
+nikElement.textContent = "12";
+namaElement.textContent = "Budi Santoso";
+ttlElement.textContent = "Yogyakarta, 1 Januari 1990";
